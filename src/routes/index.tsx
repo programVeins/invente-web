39ced4907fad354f7505383f2cc3a349e78a44c8
sabesh <sabesh5601@gmail.com>
2022-09-25 14:15:58 +0530

    Add tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,9 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 export default component$(() => {
   return (
     <div> 
-      <h1 class="header">Invente 6.0</h1>
+      <h1 class="text-3xl font-bold underline text-white">
+        Invente 6.0
+      </h1>
     </div>
   );
 });
