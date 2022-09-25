737b243a244dd605ca50d8a7c3ab018382c1f56b
sabesh <sabesh5601@gmail.com>
2022-09-25 12:29:42 +0530

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div> </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+};
